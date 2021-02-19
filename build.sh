fe2a49734d92cf84cc097f2b3e7a4f004c7b354f
Wouter Wijsman <wouter.wijsman@qbeyond.de>
2021-02-19 13:51:09 +0100

    Add basic build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd "$(dirname "${0}")"
+WORKDIR="${PWD}"
+
+FAILED=""
+
+build_package() {
+	# Make sure ${1} is set
+	if [ -z "${1}" ]; then
+		echo "No argument specified for build_package function"
+		return
+	fi
+
+	cd "$(dirname ${1})"
+	if ! makepkg -si --noconfirm; then
+		FAILED="${FAILED} ${1}"
+	fi
+	cd "${WORKDIR}"
+}
+
+for pkgbuild in $(find -type f -name PKGBUILD); do
+	build_package($pkgbuild)
+done
